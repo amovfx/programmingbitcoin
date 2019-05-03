fa12b732b352d347f723b889be6de17e8b6ebc10
amovfx <amo.vfx@protonmail.com>
2019-05-03 08:01:12 -0400

    FiniteFieldTest:     Additional error testing of singleton
    FiniteField:
        Better get set of _prime.
        Better __new__ to handle default cases and overriting the prime value.

--- BTCTutorial/finiteField.py
+++ BTCTutorial/finiteField.py
@@ -1,6 +1,6 @@
+#Copyright dwetfyw
+
 from __future__ import annotations
-from typing import Union
-from functools import wraps
 
 from math import sqrt
 from itertools import count, islice
@@ -10,41 +10,70 @@ import unittest
 #todo annotate with comments
 
 class FiniteField:
+    """Finite Field is a Singleton to contain a prime value to limit the field for future classes."""
     class __FiniteField:
         def __init__(self, prime):
-
-            self.prime = prime
+            self.setPrime(prime)
 
         def __repr__(self):
-            return f"<FiniteField ({self.prime}) >"
+            return f"<FiniteField ({self.getPrime()}) >"
 
+        def getPrime(self):
+            return self._prime
+
+        def setPrime(self, prime):
+            """Filter for prime numbers """
+            if prime > 1 and all(prime % i for i in islice(count(2), int(sqrt(prime) - 1))):
+                self._prime = prime
+            else:
+                raise ValueError(f'{prime} is not prime')
 
 
     instance = None
 
-    def __new__(cls, prime=17):
-        if prime > 1 and all(prime % i for i in islice(count(2), int(sqrt(prime) - 1))):
-            if not FiniteField.instance:
-                FiniteField.instance = FiniteField.__FiniteField(prime)
-            return FiniteField.instance
+    def __new__(cls, prime=None):
+
+
+        """Create singleton"""
+        if FiniteField.instance is None:
+            if prime is None:
+                Warning("prime value defaulting to 17.")
+                prime = 17
+            FiniteField.instance = FiniteField.__FiniteField(prime)
         else:
-            raise ValueError(f'{prime} is not prime')
+            FiniteField.instance.setPrime(prime)
+        return FiniteField.instance
+
 
 
     def __getttr__(self, name):
         return getattr(self.instance, name)
 
     def __setattr__(self, name):
-        #todo: add check for setting prime
         return setattr(self.instance, name)
 
 
 
 class FiniteFieldTest(unittest.TestCase):
+    """Various unit testing for the FiniteField class."""
 
     def test_prime(self):
+        #print ("Testing non prime")
         with self.assertRaises(ValueError):
             nonPrimeField = FiniteField(16)
-        primeField = FiniteField(17)
-        self.assertEqual(17, primeField.prime)
 
+    def test_functions(self):
+        primeField = FiniteField(17)
+        self.assertEqual(17, primeField._prime)
+        self.assertEqual(17, primeField.getPrime())
+
+    def test_Simpleton(self):
+        primeField = FiniteField(23)
+        self.assertNotEqual(17, primeField.getPrime())
+
+    def test_NoDefault(self):
+        primeField = FiniteField()
+        print (primeField.getPrime())
+
+if __name__ == "__main__":
+    unittest.main()
